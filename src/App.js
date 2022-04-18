7d24230f19c39709ef77511875424329cad9a9ab
Amirreza <itsamirrezah@gmail.com>
2022-04-18 22:49:57 +0430

    refactor homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 // import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./App.css";
-import Banner from "./components/banner/Banner";
-import Subscription from "./components/ui/Subscription/Subscription";
 
 function App() {
   return (
     <div className="App">
-      <section className="banner">
-        <Banner
-          header="Helping digital products"
-          text="Tracto is a remote contract facilitator that aims to secure, manage and ensure quality delivery for freelancers"
-        >
-          The workplace for remote contracts gigs.
-        </Banner>
-        <Subscription />
-      </section>
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,22 @@
-import React, { Fragment } from 'react'
-import Section2 from '../components/sections/section-2/Section2'
-
+import React, { Fragment } from "react";
+import Section2 from "../components/sections/section-2/Section2";
+import Banner from "../components/banner/Banner";
+import Subscription from "../components/ui/Subscription/Subscription";
+import styles from "./Homepage.module.css";
 export default function HomePage() {
   return (
     <Fragment>
-        <Section2/>
+      <section className={styles.banner}>
+        <Banner
+          header="Helping digital products"
+          text="Tracto is a remote contract facilitator that aims to secure, manage and ensure quality delivery for freelancers"
+        >
+          The workplace for remote contracts gigs.
+        </Banner>
+        <Subscription />
+      </section>
+
+      <Section2 />
     </Fragment>
-  )
+  );
 }
