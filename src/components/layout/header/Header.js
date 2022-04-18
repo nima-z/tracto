7dddc4b615fa80f142543697f830c31dc389e250
nima-z <nima13zare@gmail.com>
2022-04-18 18:51:16 +0430

    layout 2

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,5 +1,19 @@
 import React from "react";
+import Logo from "../../UI/Logo";
+import Spacer from "../../UI/Spacer";
+import Navigation from "../navigation/Navigation";
+
+import styles from "./Header.module.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <Navigation />
+      <Spacer />
+      <div>button</div>
+    </div>
+  );
 }
