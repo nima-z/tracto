74bab3c9fea44f40807fd31db56944c64df07a6e
nima-z <nima13zare@gmail.com>
2022-04-19 17:47:01 +0430

    banner & features done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <AboutUs />
     </div>
   );
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import About1 from "../components/sections/about-1/About1";
+import About2 from "../components/sections/about-2/About2";
+
+export default function AboutUs() {
+  return (
+    <Fragment>
+      <About1 />
+      <About2 />
+    </Fragment>
+  );
+}
