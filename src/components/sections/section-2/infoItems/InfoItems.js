15b2e3c01e5596d40c6b7e2971c64fc395d324ee
nima-z <nima13zare@gmail.com>
2022-04-19 10:51:37 +0430

    section2 Completed

---
+++ src/components/sections/section-2/infoItems/InfoItems.js
@@ -0,0 +1,41 @@
+import styles from './InfoItems.module.css'
+import Chart from '../../../ui/icons/Chart'
+import Money from '../../../ui/icons/Money'
+import Tags from '../../../ui/icons/Tags'
+
+
+export default function InfoItem({icon, title, desc}) {
+
+  return (
+    <div className={styles.container}>    
+      <div className={styles.infoItem}>
+        <div className={styles.icon}>
+            <Chart/>
+        </div>
+        <div className={styles.info}>
+            <h2>35x</h2>
+            <p>Efficient than any Local Means</p>
+        </div>
+      </div>
+      <div className={styles.infoItem}>
+        <div className={styles.icon}>
+            <Money/>
+        </div>
+        <div className={styles.info}>
+            <h2>$0.00</h2>
+            <p>For Choosing the best way of Contract</p>
+        </div>
+      </div>
+      <div className={styles.infoItem}>
+        <div className={styles.icon}>
+            <Tags/>
+        </div>
+        <div className={styles.info}>
+            <h2>0%</h2>
+            <p>For Choosing the best way of Contract</p>
+        </div>
+      </div>
+    </div>
+
+  )
+}
