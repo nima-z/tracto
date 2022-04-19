220c66373b97e6e32278dfd2d0e11e27d5d9fb5e
Amirreza <itsamirrezah@gmail.com>
2022-04-19 09:52:54 +0430

    section3 overview

---
+++ src/components/sections/section-3/Section3.js
@@ -0,0 +1,10 @@
+import styles from "./Section3.module.css";
+
+export default function Section3() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.headline}></div>
+      <div className={styles.features}></div>
+    </div>
+  );
+}
