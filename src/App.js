f915cf6a88bdcf9d81224a0cbb6e2926cb1c4731
nima-z <nima13zare@gmail.com>
2022-04-19 18:24:20 +0430

    privacy added to page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 import "./App.css";
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="App">
       {/* <HomePage /> */}
-      <AboutUs />
+      {/* <AboutUs /> */}
+      <PrivacyPolicy />
     </div>
   );
 }
