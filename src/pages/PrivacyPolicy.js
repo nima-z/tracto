465bf69db6c5ebbabd90042a03ea97d46989870a
nima-z <nima13zare@gmail.com>
2022-04-20 11:04:13 +0430

    privacy list added

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,5 +1,12 @@
-import React from "react";
+import { Fragment } from "react";
+import Privacy1 from "../components/sections/privacy-1/Privacy1";
+import Privacy2 from "../components/sections/privacy-2/Privacy2";
 
 export default function PrivacyPolicy() {
-  return <div>PrivacyPolicy</div>;
+  return (
+    <Fragment>
+      <Privacy1 />
+      <Privacy2 />
+    </Fragment>
+  );
 }
