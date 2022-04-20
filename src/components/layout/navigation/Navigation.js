a6cd2fe6871e8b5220770384d62d08d7e8bf6deb
nima-z <nima13zare@gmail.com>
2022-04-20 18:59:09 +0430

    scroll added

--- src/components/layout/navigation/Navigation.js
+++ src/components/layout/navigation/Navigation.js
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router-dom";
+
 import styles from "./Navigation.module.css";
 
 export default function Navigation() {
   return (
     <nav className={styles.container}>
-      <NavLink to="/">How it Works</NavLink>
+      <NavLink to="/#features">How it Works</NavLink>
+
       <NavLink to="/about">About Us</NavLink>
-      <a href="#testimonial">Testimonials</a>
+      <NavLink to="/#testimonial">Testimonials</NavLink>
     </nav>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,18 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Section1 from "../components/sections/section-1/Section1";
 import Section2 from "../components/sections/section-2/Section2";
 import Section3 from "../components/sections/section-3/Section3";
 import Section4 from "../components/sections/section-4/Section4";
 
 export default function HomePage() {
+  const location = useLocation();
+  useEffect(() => {
+    const element = document.getElementById(location.hash.slice(1));
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [location]);
   return (
     <Fragment>
       <Section1 />
