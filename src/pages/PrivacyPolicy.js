01303e2ce0270769da1a2b1499a45f977e5c5b6c
nima-z <nima13zare@gmail.com>
2022-04-21 20:10:17 +0430

    privacy policy scroll up fixed

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,9 +1,18 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Privacy1 from "../components/sections/privacy-1/Privacy1";
 import Privacy2 from "../components/sections/privacy-2/Privacy2";
 import PromotionSection from "../components/sections/promotion/PromotionSection";
 
 export default function PrivacyPolicy() {
+  const location = useLocation();
+  useEffect(() => {
+    const element = document.getElementById(location.hash.slice(1));
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [location]);
+
   return (
     <Fragment>
       <Privacy1 />
