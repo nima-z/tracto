647752d70febab5098a000a5cbf08e82d7f638c4
nima-z <nima13zare@gmail.com>
2022-04-21 10:35:31 +0430

    image added + responsive

--- src/components/sections/section-1/Section1.js
+++ src/components/sections/section-1/Section1.js
@@ -1,18 +1,21 @@
 import Banner from "../../banner/Banner";
 import Subscription from "../../ui/Subscription/Subscription";
 
-import styles from './Section1.module.css'
+import styles from "./Section1.module.css";
 
 export default function Section1() {
   return (
     <section className={styles.banner}>
-        <Banner
-          header="Helping digital products"
-          text="Tracto is a remote contract facilitator that aims to secure, manage and ensure quality delivery for freelancers"
-        >
-          The workplace for remote contracts gigs.
-        </Banner>
-        <Subscription />
-      </section>
-  )
+      <Banner
+        header="Helping digital products"
+        text="Tracto is a remote contract facilitator that aims to secure, manage and ensure quality delivery for freelancers"
+      >
+        The workplace for remote contracts gigs.
+      </Banner>
+      <Subscription />
+      <div className={styles.floatImage}>
+        <img src="/images/SignUp.png" alt="signup" />
+      </div>
+    </section>
+  );
 }
